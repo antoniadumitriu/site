e5abb0f7cbcfc7bd82807f4c7652644a45c3c6bf
antoniadumitriu <adumitriu2000@gmail.com>
2023-11-13 15:14:12 +0000

    Updates

---
+++ precache-manifest.c7008435da0cadc764f8dc6b3c33c344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "988a435c872323e98242",
+    "url": "/site/static/css/main.3ad67204.chunk.css"
+  },
+  {
+    "revision": "988a435c872323e98242",
+    "url": "/site/static/js/main.988a435c.chunk.js"
+  },
+  {
+    "revision": "c4b54bfd49c02f9eaead",
+    "url": "/site/static/js/runtime~main.c4b54bfd.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/site/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "9c4540f4b1272c8c01b011c040ac176c",
+    "url": "/site/index.html"
+  }
+];
